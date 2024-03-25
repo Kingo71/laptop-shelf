94188df1def5d0d3892bf8511580863c73bc779e
kingo71 <angelo.kingo@gmail.com>
2024-03-25 13:45:10 +0100

    Date and time input

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,38 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 RTC_DS3231 rtc;
 
 
+void dateTimeInput(){
+
+  String date_string = Serial.readStringUntil('\n');
+  String time_string = Serial.readStringUntil('\n');
+
+  int year = date_string.substring(0, 4).toInt();
+  int month = date_string.substring(5, 7).toInt();
+  int day = date_string.substring(8, 10).toInt();
+  int hour = time_string.substring(0, 2).toInt();
+  int minute = time_string.substring(3, 5).toInt();
+  int second = time_string.substring(6, 8).toInt();
+
+  DateTime dt(year, month, day, hour, minute, second);
+
+  if (dt.year() < 2000 || dt.year() > 2099 || dt.month() < 1 || dt.month() > 12 || dt.day() < 1 || dt.day() > 31 || dt.hour() < 0 || dt.hour() > 23 || dt.minute() < 0 || dt.minute() > 59 || dt.second() < 0 || dt.second() > 59) {
+    Serial.println("Invalid date or time entered!");
+  } else {
+    rtc.adjust(dt);
+    Serial.println("RTC date and time set!");
+
+    // Print the date just stored in the RTC
+    Serial.print("Date just stored in RTC: ");
+    Serial.print(rtc.now().year(), DEC);
+    Serial.print("-");
+    Serial.print(rtc.now().month(), DEC);
+    Serial.print("-");
+    Serial.print(rtc.now().day(), DEC);
+    Serial.println();
+
+ }
+}
+
 void setup() {
   
   strip.begin();
@@ -58,7 +90,7 @@ void loop() {
   }
 
   // Determine which week of the month today falls into
-  // int currentDay = 20; //now.day();
+  int currentDay = now.day();
   int weekOfMonth = 0;
   if (currentDay >= firstMonday && currentDay < (firstMonday + 7))
     weekOfMonth = 1;
@@ -139,22 +171,8 @@ void loop() {
   }
   
   if (Serial.available()) {
-    String input = Serial.readStringUntil('\n');
-    currentDay = input.toInt();
-
-    // Print the current date and time
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
+    
+    dateTimeInput();
 
   }
 
